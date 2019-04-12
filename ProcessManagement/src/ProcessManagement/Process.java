bb0ba467c893b21f270f5bea1fbcbdc22f546455
Taylor McLaughlin <44411229+tmclau96@users.noreply.github.com>
2019-04-12 12:48:09 -0400

    Update Process.java
    added attributes for a process

--- ProcessManagement/src/ProcessManagement/Process.java
+++ ProcessManagement/src/ProcessManagement/Process.java
@@ -3,12 +3,33 @@ package ProcessManagement;
 public class Process {
     private int pid;
     private String state;
+    private int arrival_time;
+    private int run_time;
     
-    public Process(int pid, String state)
+    public Process(int pid, String state, int arrival_time, int run_time)
     {
         this.pid = pid;
         this.state = state;
+        this.arrival_time = arrival_time;
+        this.run_time = run_time;
     }//end constructor
+    
+    public Process(Process p)
+    {
+        this.pid = p.getPID();
+        this.state = p.getState();
+        this.arrival_time = p.getArrivalTime();
+        this.run_time = p.getRunTime();
+    }//end copy constructor
+   
+    //setter methods
+    
+    public void setState(String state)
+    {
+        this.state = state;
+    }//end set state
+   
+    //getter methods
     
     public int getPID()
     {
@@ -19,4 +40,17 @@ public class Process {
     {
         return state;
     }//end get state
+    
+    public int getArrivalTime()
+    {
+        return arrival_time;
+    }//end get arrival time
+    
+    public int getRunTime()
+    {
+        return run_time;
+    }//end get run time
+    
+
 }//end class
+
