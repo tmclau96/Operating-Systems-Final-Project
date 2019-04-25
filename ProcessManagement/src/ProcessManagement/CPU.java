fd95661d389222448535e565167242fb0010c1c6
jjacks52 <43050700+jjacks52@users.noreply.github.com>
2019-04-24 21:18:36 -0400

    Update CPU.java

--- ProcessManagement/src/ProcessManagement/CPU.java
+++ ProcessManagement/src/ProcessManagement/CPU.java
@@ -10,7 +10,18 @@ package ProcessManagement;
  * @author anastasiya
  */
 public class CPU {
+    private int sleepTime;
+	
+    public CPU(int sleepTime)
+    {
+    	this.sleepTime=sleepTime;
+    }
     
+    public void sleep()
+    {
+    	Thread.sleep(sleepTime);
+    	System.out.println(Thread.currentThread().getName()+" The task executed at "+ new Date());
+    }
     // make a Thread sleep for run_time millis(?)
     
 }
